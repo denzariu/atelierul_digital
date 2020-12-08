697b944b6ca306961fa5d30550e082edbfba0742
GamePhobia <40743579+GamePhobia@users.noreply.github.com>
2020-12-08 18:29:55 +0200

    Curs 2
    - Project Week 2

---
+++ app/src/main/java/com/example/apptest/week2/ViewsActivity.java
@@ -0,0 +1,16 @@
+package com.example.apptest.week2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.apptest.R;
+
+public class ViewsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_views);
+    }
+}
